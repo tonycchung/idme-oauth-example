d34811f0da0e285e756946938d205da374eb5f4f
Tony Chung <anthonyuchung@gmail.com>
2015-01-31 23:07:59 -0800

    Add foundation for style

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,11 +2,13 @@ class SessionsController < ApplicationController
   def create
     user = User.from_omniauth(request.env["omniauth.auth"])
     session[:user_id] = user.id
-    redirect_to root_url, notice: "Signed in!"
+    flash[:notice] = "Signed in!"
+    redirect_to root_url
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_url, notice: "Signed out!"
+    flash[:notice] = "Signed out!"
+    redirect_to root_url
   end
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,5 @@
+OmniAuth.config.logger = Rails.logger
+
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :idme, ENV["IDME_KEY"], ENV["IDME_SECRET"]
 end
