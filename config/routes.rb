2c65c3fc5e463d98f16e6a160f8dc7a1f9b0e6c4
Tony Chung <anthonyuchung@gmail.com>
2015-01-31 21:48:26 -0800

    Add signout ability

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'welcome#index'
   get 'auth/idme/callback' => 'sessions#create'
+  get 'signout' => 'sessions#destroy', as: :signout
+  get 'auth/failure' => redirect('/')
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
